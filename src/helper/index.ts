51d97167baf0dc4e55376ebc0bf2712d8218f624
Hoon Kim <hoonsubin@icloud.com>
2020-09-27 23:10:18 -0700

    added swagger ui to root

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,4 +1,4 @@
-export * as PlasmSubscan from './plasmSubscan';
+export * as PlasmSubscan from './plasmEventScan';
 export * as Utils from './utils';
 export * as PlasmUtils from './plasmUtils';
 export * as EthLockdrop from './ethereum';
